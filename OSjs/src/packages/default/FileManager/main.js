bd711c02664c2efad601f2064b018ddb3f5a0716
root <root@shmc-server.fritz.box>
2017-05-29 15:33:31 +0200

    small changes to FileManager

--- OSjs/src/packages/default/FileManager/main.js
+++ OSjs/src/packages/default/FileManager/main.js
@@ -38,14 +38,14 @@
 
   function ApplicationFileManager(args, metadata) {
     Application.apply(this, ['ApplicationFileManager', args, metadata, {
-      src: 'https://shmc-server:443',
+      src: 'https://shmc-server',
       title: metadata.name,
       icon: metadata.icon,
       width: 640,
       height: 480,
-      allow_resize: false,
-      allow_restore: false,
-      allow_maximize: false
+      allow_resize: true,
+      allow_restore: true,
+      allow_maximize: true
     }]);
   }
 
