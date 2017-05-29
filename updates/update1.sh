0301f93c667a65ef775758ee678d078d1af64ea5
root <root@shmc-server.fritz.box>
2017-05-29 15:00:48 +0200

    fixed updatescript

--- updates/update1.sh
+++ updates/update1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cp ./files/osjs.service /etc/systemd/system/osjs.service
-cp ./files/repsonse.php /usr/share/webapps/nextcloud/lib/private/legacy/response.php
+cp /SHMC/updates/files/osjs.service /etc/systemd/system/osjs.service
+cp /SHMC/updates/files/response.php /usr/share/webapps/nextcloud/lib/private/legacy/response.php
 sudo -u http /usr/share/webapps/nextcloud/occ config:app:set "richdocuments" '"wopi_url": "https://shmc-server"'
-
-
+systemctl daemon-reload
 reboot
