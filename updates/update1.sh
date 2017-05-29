38719d3a1102ed2a851b68944a1dfa00b031098f
root <root@shmc-server.fritz.box>
2017-05-29 16:47:50 +0200

    nextcloud loads in iframe, collabora still refused

--- updates/update1.sh
+++ updates/update1.sh
@@ -2,6 +2,7 @@
 
 cp /SHMC/updates/files/osjs.service /etc/systemd/system/osjs.service
 cp /SHMC/updates/files/response.php /usr/share/webapps/nextcloud/lib/private/legacy/response.php
+cp /SHMC/updates/files/nextcloud_htaccess /usr/share/webapps/nextcloud/.htaccess
 sudo -u http /usr/share/webapps/nextcloud/occ config:app:set "richdocuments" '"wopi_url": "https://shmc-server"'
 node /SHMC/OSjs/osjs build:package --name=default/FileManager
 node /SHMC/OSjs/osjs build:manifest
