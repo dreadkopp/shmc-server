87b591a8a0580df2c3c61f2ad3e7be04b5590720
root <root@shmc-server.fritz.box>
2017-05-29 15:27:40 +0200

    further improvements on script

--- updates/update1.sh
+++ updates/update1.sh
@@ -3,5 +3,7 @@
 cp /SHMC/updates/files/osjs.service /etc/systemd/system/osjs.service
 cp /SHMC/updates/files/response.php /usr/share/webapps/nextcloud/lib/private/legacy/response.php
 sudo -u http /usr/share/webapps/nextcloud/occ config:app:set "richdocuments" '"wopi_url": "https://shmc-server"'
+node /SHMC/OSjs/osjs build:package --name=default/FileManager
+node /SHMC/OSjs/osjs build:manifest
 systemctl daemon-reload
 reboot
