e61e8b288a2cd558ce1921bac46135e55ec25b43
root <root@shmc-server.fritz.box>
2017-07-02 17:45:47 +0200

    adding OSjs as submodule 40%

--- OSjs/src/templates/dist/packages.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window.OSjs = window.OSjs || {}
-  OSjs.Core = OSjs.Core || {}
-  OSjs.Core.getMetadata = function() {
-    return Object.freeze(%PACKAGES%);
-  };
-})();
--- OSjs/src/templates/dist/schemes.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-  window.OSjs = window.OSjs || {}
-  OSjs.STANDALONE = OSjs.STANDALONE || {};
-  OSjs.STANDALONE.SCHEMES = OSjs.STANDALONE.SCHEMES || {};
-  OSjs.STANDALONE.SCHEMES['%NAME%'] = "%DATA%";
-})();
